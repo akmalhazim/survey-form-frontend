6a42afb7289a5ba5943aa6553272ae2ff37dcaa6
Muhammad Akmal Hazim Hafizul Rasydan <akmalhazim@inspyrehosting.com>
2018-09-01 00:35:55 +0800

    Initial MVP Usable

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div id="app">
     <!--<img alt="Vue logo" src="./assets/logo.png">-->
     <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <h1>Pre-Course Questionaire</h1>
+    <p style="margin:0;font-size:16px;">Application of Windows Servers</p>
+    <br>
+    <br>
+    <br>
     <Form backend="http://survey-backend.test"></Form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Form from './components/form'
-import axios from 'axios'
 
 export default {
   name: 'app',
--- src/components/form.vue
+++ src/components/form.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center text-left">
              <div class="col-lg-6">
+                 <label for="kad">Nombor kad matriks</label>
+                 <input v-model="user_id" class="form-control form-control-md" id="kad" type="text" placeholder="CB180xxx">
+                 <br>
                  <div v-for="(question, qIndex) in questions" class="text-left form-check">
                      <!--<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>-->
                      <label class="form-check-label">
@@ -16,22 +19,24 @@
                      <br>
                      <br>
                  </div>
+                 <button @click="submit" class="btn btn-success btn-block">I'm done!</button>
+                 <br>
+                 <br>
              </div>
         </div>
     </div>
 </template>
 <script>
     import axios from 'axios'
+    import swal from 'sweetalert2'
     export default {
         props: ['backend'],
         data() {
           return {
               questions: [],
-              answer: []
+              answer: [],
+              user_id: ''
           }
-        },
-        computed: {
-
         },
         methods: {
             getQuestion: function() {
@@ -45,6 +50,50 @@
                     })
                     .catch(err => {
                         console.log(err)
+                    })
+            },
+            submit: function() {
+                axios.post(this.backend+'/api/submit', {
+                    data: this.answer,
+                    user_id: this.user_id
+                })
+                    .then(res => {
+                        console.log(res)
+                        if(res.data.result >= 60) {
+                            swal(
+                                'Congrats!',
+                                'You scored <strong>'+res.data.result+'%</strong> overall!',
+                                'success'
+                            )
+                        } else if(res.data.result < 60 && res.data.result >= 21) {
+                            swal(
+                                'You can do better!',
+                                'You scored <strong>'+res.data.result+'%</strong> overall!',
+                                'success'
+                            )
+                        } else if(res.data.result <= 21) {
+                            swal(
+                                'Try to pay more attention in class!',
+                                'You scored <strong>'+res.data.result+'%</strong> overall!',
+                                'success'
+                            )
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err.response.status)
+                        if(err.response.status === 422) {
+                            swal(
+                                'Incomplete form!',
+                                'Please complete the form!',
+                                'error'
+                            )
+                        }
+                    })
+                    .finally(res => {
+                        this.answer = []
+                        this.questions.forEach(question => {
+                            this.answer.push({question_id: question.question_id, answer: ''});
+                        })
                     })
             }
         },
